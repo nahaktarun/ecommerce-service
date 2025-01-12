20821d9e05c4779098ca0ac284572e6ffbe696de
Tarun Nahak <nahaktarun@Taruns-MacBook-Pro.local>
2025-01-12 21:29:58 +0530

    Added products get and post endpoints

--- src/server.js
+++ src/server.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const morgan = require("morgan");
 const connectDB = require("./config/db");
-
+const productRoutes = require('./routes/productRoutes')
+require('dotenv').config()
 const app = express();
 app.use(morgan("dev"));
+app.use(express.json());
 connectDB()
+
+const PORT = process.env.PORT || 3000;
+
+// Home route
 app.get("/", (req, res) => {
   res.send("E-commerce service backend is running....");
 });
-app.listen(3000, () => {
-  console.log("`Server listening to the port 3000.");
+
+// products route
+app.use("/api/products", productRoutes)
+app.listen(PORT, () => {
+  console.log(`Server listening to the port ${PORT}.`);
 });
